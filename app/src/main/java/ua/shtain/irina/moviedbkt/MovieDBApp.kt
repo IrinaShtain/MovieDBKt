0470e30d1761ab54c6ff92ad6ef45b0f24841315
IrinaShtain <shtainirina@gmail.com>
2018-01-30 14:06:04 +0200

    add necessary libraries

---
+++ app/src/main/java/ua/shtain/irina/moviedbkt/MovieDBApp.kt
@@ -0,0 +1,15 @@
+package ua.shtain.irina.moviedbkt
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+
+/**
+ * Created by Irina Shtain on 30.01.2018.
+ */
+class MovieDBApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
+}
